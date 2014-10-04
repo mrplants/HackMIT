2a6370d1ca5ce410aeab0f582ace535c9e4b39fb
Sean T Fitzgerald <fitzgerald.sean.t@gmail.com>
2014-10-04 13:00:45 -0400

    Initial Commit

---
+++ Arcade Game/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Arcade Game
+//
+//  Created by Sean Fitzgerald on 10/4/14.
+//  Copyright (c) 2014 Sean T Fitzgerald. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
